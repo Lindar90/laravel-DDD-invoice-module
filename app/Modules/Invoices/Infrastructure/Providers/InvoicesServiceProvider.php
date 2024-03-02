4ae8826827ebcc6845647514d2153987fbe87d79
Denis Marusevich <denismarusevich@cloud.upwork.com>
2024-03-02 18:56:02 +0100

    Connect interface to implementations:
    - invoice facade
    - invoice repo

---
+++ app/Modules/Invoices/Infrastructure/Providers/InvoicesServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Invoices\Infrastructure\Providers;
+
+use App\Modules\Invoices\API\InvoiceFacadeInterface;
+use App\Modules\Invoices\Application\InvoiceFacade;
+use App\Modules\Invoices\Domain\Repositories\InvoiceRepositoryInterface;
+use App\Modules\Invoices\Infrastructure\Database\Repositories\InvoiceRepository;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class InvoicesServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public function register(): void
+    {
+        $this->app->scoped(InvoiceFacadeInterface::class, InvoiceFacade::class);
+        $this->app->bind(InvoiceRepositoryInterface::class, InvoiceRepository::class);
+    }
+
+    /** @return array<class-string> */
+    public function provides(): array
+    {
+        return [
+            InvoiceFacadeInterface::class,
+        ];
+    }
+}
