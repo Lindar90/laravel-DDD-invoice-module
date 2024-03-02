dde2a1865fcbdd97c6e3def985e3efe4c486e257
Denis Marusevich <denismarusevich@cloud.upwork.com>
2024-03-02 18:51:41 +0100

    Invoice repository:
    - interface
    - implementation
    - domain exception

---
+++ app/Modules/Invoices/Domain/Repositories/InvoiceRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Invoices\Domain\Repositories;
+
+use App\Modules\Invoices\Domain\Exceptions\InvoiceNotFoundException;
+use App\Modules\Invoices\Domain\Models\InvoiceModel;
+
+interface InvoiceRepositoryInterface
+{
+    /**
+     * @throws InvoiceNotFoundException
+     */
+    public function getDetails(string $id): InvoiceModel;
+}
---
+++ app/Modules/Invoices/Infrastructure/Database/Repositories/InvoiceRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Invoices\Infrastructure\Database\Repositories;
+
+use App\Modules\Invoices\Domain\Exceptions\InvoiceNotFoundException;
+use App\Modules\Invoices\Domain\Models\InvoiceModel;
+use App\Modules\Invoices\Domain\Repositories\InvoiceRepositoryInterface;
+use App\Modules\Invoices\Infrastructure\Database\Entities\InvoiceEntity;
+use App\Modules\Invoices\Infrastructure\Database\Mappers\InvoiceEntityToModelMapper;
+
+class InvoiceRepository implements InvoiceRepositoryInterface
+{
+    /**
+     * @throws InvoiceNotFoundException
+     */
+    public function getDetails(string $id): InvoiceModel
+    {
+        /** @var InvoiceEntity|null $invoice */
+        $invoice = InvoiceEntity::with(['company', 'productLines', 'productLines.product'])->find($id);
+
+        if (null === $invoice) {
+            throw new InvoiceNotFoundException($id);
+        }
+
+        return InvoiceEntityToModelMapper::map($invoice);
+    }
+}
