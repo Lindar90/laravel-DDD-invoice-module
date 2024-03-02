9d53d09b66e1d68de4f0a3f023ad3524d8906a67
Denis Marusevich <denismarusevich@cloud.upwork.com>
2024-03-02 20:12:11 +0100

    Facade and controller logic for approval

--- app/Modules/Invoices/Application/InvoiceFacade.php
+++ app/Modules/Invoices/Application/InvoiceFacade.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace App\Modules\Invoices\Application;
 
+use App\Modules\Approval\Api\ApprovalFacadeInterface;
+use App\Modules\Approval\Api\Dto\ApprovalDto;
+use App\Modules\Invoices\API\DTO\InvoiceApproveRequestDTO;
+use App\Modules\Invoices\API\DTO\InvoiceApproveResponseDTO;
 use App\Modules\Invoices\API\DTO\InvoiceDetailsRequestDTO;
 use App\Modules\Invoices\API\DTO\InvoiceDetailsResponseDTO;
 use App\Modules\Invoices\API\InvoiceFacadeInterface;
-use App\Modules\Invoices\Application\Mappers\InvoiceModelToResponseDTOMapper;
+use App\Modules\Invoices\Application\Mappers\InvoiceModelToInvoiceApproveResponseDTO;
+use App\Modules\Invoices\Application\Mappers\InvoiceModelToInvoiceDetailsResponseDTOMapper;
+use App\Modules\Invoices\Domain\Exceptions\InvoiceCanNotBeApprovedException;
 use App\Modules\Invoices\Domain\Exceptions\InvoiceNotFoundException;
 use App\Modules\Invoices\Domain\Repositories\InvoiceRepositoryInterface;
+use Illuminate\Log\LogManager;
+use LogicException;
 
 readonly class InvoiceFacade implements InvoiceFacadeInterface
 {
     public function __construct(
-        private InvoiceRepositoryInterface $invoiceRepository
+        private InvoiceRepositoryInterface $invoiceRepository,
+        private ApprovalFacadeInterface $approvalFacade,
+        private LogManager $logger,
     ) {
     }
 
@@ -23,8 +33,33 @@ readonly class InvoiceFacade implements InvoiceFacadeInterface
      */
     public function getDetails(InvoiceDetailsRequestDTO $dto): InvoiceDetailsResponseDTO
     {
+        $invoiceModel = $this->invoiceRepository->getDetails($dto->getId());
+
+        return InvoiceModelToInvoiceDetailsResponseDTOMapper::map($invoiceModel);
+    }
+
+    /**
+     * @throws InvoiceCanNotBeApprovedException|InvoiceNotFoundException
+     */
+    public function approve(InvoiceApproveRequestDTO $dto): InvoiceApproveResponseDTO
+    {
         $invoice = $this->invoiceRepository->getDetails($dto->getId());
 
-        return InvoiceModelToResponseDTOMapper::map($invoice);
+        $approvalDto = new ApprovalDto(
+            id: $invoice->getId(),
+            status: $invoice->getStatus(),
+            entity: 'invoice',
+        );
+
+        try {
+            $this->approvalFacade->approve($approvalDto);
+
+            $invoiceModel = $this->invoiceRepository->approve($dto->getId());
+
+            return  InvoiceModelToInvoiceApproveResponseDTO::map($invoiceModel);
+        } catch (LogicException $e) {
+            $this->logger->warning($e->getMessage());
+            throw new InvoiceCanNotBeApprovedException($dto->getId());
+        }
     }
 }
--- app/Modules/Invoices/Infrastructure/Http/Controllers/InvoiceApproveController.php
+++ app/Modules/Invoices/Infrastructure/Http/Controllers/InvoiceApproveController.php
@@ -4,10 +4,36 @@ declare(strict_types=1);
 
 namespace App\Modules\Invoices\Infrastructure\Http\Controllers;
 
-class InvoiceApproveController
+use App\Infrastructure\Http\Response\JsonBadRequestResponse;
+use App\Infrastructure\Http\Response\JsonErrorResponse;
+use App\Infrastructure\Http\Response\JsonSuccessResponse;
+use App\Modules\Invoices\API\DTO\InvoiceApproveRequestDTO;
+use App\Modules\Invoices\API\InvoiceFacadeInterface;
+use App\Modules\Invoices\Domain\Exceptions\InvoiceCanNotBeApprovedException;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+readonly class InvoiceApproveController
 {
-    public function __invoke(string $id): \Illuminate\Http\JsonResponse
+    public function __construct(
+        private InvoiceFacadeInterface $invoiceFacade
+    ) {
+    }
+
+    public function __invoke(string $id): JsonResponse
     {
-        return response()->json(['id' => $id]);
+        $dto = new InvoiceApproveRequestDTO($id);
+
+        try {
+            $responseDTO = $this->invoiceFacade->approve($dto);
+
+            return new JsonSuccessResponse($responseDTO->toArray());
+        } catch (InvoiceCanNotBeApprovedException $e) {
+            return new JsonBadRequestResponse('Invoice can not be approved', [
+                'id' => 'Invoice can not be approved',
+            ]);
+        } catch (Exception $e) {
+            return new JsonErrorResponse();
+        }
     }
 }
